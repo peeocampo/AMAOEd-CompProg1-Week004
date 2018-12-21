d2048aa04dced86f9753328ad161019025d450a5
peeocampo <45783874+peeocampo@users.noreply.github.com>
2018-12-22 00:11:01 +0800

    Add files via upload

---
+++ AMAOEd-CompProg1-Week005/Budget.cpp
@@ -0,0 +1,46 @@
+/* ****************************************************************
+ * @author: <Paolo Ocampo>
+ * @app name: <Lab Exercises 004>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    float floatBudget;
+    cout << "Enter your Budget: ";
+    cin >> floatBudget;
+    cout << floatBudget * floatBudget << "\n";
+
+    cin.clear();
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
---
+++ AMAOEd-CompProg1-Week005/Section.cpp
@@ -0,0 +1,46 @@
+/* ****************************************************************
+ * @author: <Paolo Ocampo>
+ * @app name: <Lab Exercises 004>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    string mySection;
+    cout << "Enter your section: ";
+    cin >> mySection;
+    cout << "*** Section: " << mySection << " ***\n";
+
+    cin.clear();
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
